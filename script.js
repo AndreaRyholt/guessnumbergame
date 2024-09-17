cca7c85333a457909dd00a40a77dca237e9f1b05
AndreaRyholt <anra0003@stud.kea.dk>
2024-09-17 19:54:54 +0200

    Forvirret

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+
+
 console.log("jeg er i guessnumber")
+console.log("rydapie")
 const lblMessage = document.querySelector(".message")
 console.log(lblMessage)
 console.log(lblMessage.textContent)
@@ -9,3 +12,11 @@ console.log(lblNumber)
 const lblScore = document.querySelector(".score")
 console.log(lblScore)
 
+const inpGuess = document.querySelector(".guess")
+out(inpGuess)
+
+const secretNumber = Math.random()
+
+Math.trunc(Math.random()*20)
+
+lblNumber.textContent = secretNumber
