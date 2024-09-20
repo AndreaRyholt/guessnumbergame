8eb3fd832db081979b29c2799dba6df34cad521a
AndreaRyholt <anra0003@stud.kea.dk>
2024-09-20 14:48:54 +0200

    Style-css ændret + tilføjet nyt scoresystem out(inpGuess) fungerer ikke

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
-
 console.log("jeg er i guessnumber")
-console.log("rydapie")
+
+//bliver ved med at få "ReferenceError: document is not defined" når jeg kører program
+
 const lblMessage = document.querySelector(".message")
 console.log(lblMessage)
 console.log(lblMessage.textContent)
@@ -9,14 +9,34 @@ console.log(lblMessage.textContent)
 const lblNumber = document.querySelector(".number")
 console.log(lblNumber)
 
-const lblScore = document.querySelector(".score")
-console.log(lblScore)
+
+
+const lblYouScore = document.querySelector(".youscore")
+console.log(lblYouScore)
+
+const lblComputerScore = document.querySelector(".computerscore")
+console.log(lblComputerScore)
+
+
 
 const inpGuess = document.querySelector(".guess")
-out(inpGuess)
+//out(inpGuess) //fungerer ikke?
 
-const secretNumber = Math.random()
-
-Math.trunc(Math.random()*20)
+const secretNumber = Math.trunc(Math.random()*20)
 
 lblNumber.textContent = secretNumber
+
+let youscore = 0
+let computerscore = 0
+
+if (!guess) {
+    lblMessage.textContent = '?? Try again'
+    youLose(computerscore+1)
+} else if (guess === secretNumber) {
+    lblMessage.textContent = '?? You guessed correctly!'
+    youWin(youscore+1)
+} else if (guess > secretNumber) {
+    lblMessage.textContent = "too high"
+} else if (guess < secretNumber) {
+    lblMessage.textContent = "too low"
+}
